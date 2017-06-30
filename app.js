fcaef8911b5fc3728150bc1ea38abf5a28ca6ab8
jamestthompson3 <jamestthompson3@gmail.com>
2017-06-30 15:04:22 -0700

    ready for heroku

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var config = require("./config");
 var https = require("https");
 var xmljs = require("libxmljs");
+var Client = require('node-rest-client').Client;
+var parseJson = require('parse-json');
+var json = '{\n\t"foo": true,\n}';
 
 // Init Slack client
 
@@ -24,9 +27,16 @@ app.listen(config.api.port, function() {
 
 app.post("/hello", function(request, res) {
   postSlackThings(function(result) {
+    console.log("post received.")
     res.send("slack message sent")
   });
 });
+app.get("/test", function(request ,res) {
+  getViimaInfo(function(result) {
+    res.send("slack message sent")
+  });
+});
+// Utility functions
 function postSlackThings(things, cb) {
   var hello = "hello <!channel|channel>! I'm alive!";
   var messages = {
@@ -38,5 +48,21 @@ function postSlackThings(things, cb) {
     if(err !== null) {
       console.log(err, result);
     }
+  });
+}
+function getViimaInfo() {
+  var client = new Client();
+  client.get("https://app.viima.com/api/customers/1410/activities/", function (data, response) {
+    console.log(data.results.length);
+    var data = data.results[0]
+    var slack_message = {
+      text: data.fullname + " created " + data.name + " on Viima! Check it out at " + data.url,
+      channel: config.slack.channel
+    };
+    slack.notify(slack_message, function(err, result) {
+      if(err != null) {
+        console.log(err, result);
+      }
+    });
   });
 }
