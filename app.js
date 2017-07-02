ca5d8cae9495b3833f25c2e26652ba091b05940f
jamestthompson3 <jamestthompson3@gmail.com>
2017-07-02 16:22:44 -0700

    THE BOT IS ALIVE

--- app.js
+++ app.js
@@ -65,24 +65,65 @@ function getViimaInfo() {
   client.get("https://app.viima.com/api/customers/1410/activities/", function (data, response) {
     var data = data.results
     var id_array = data.map(function(i) {
-      return i.id });
-    console.log(id_array);
+      return i.id
+    });
 
-    // firebase.database().ref("items").once("value").then((snapshot) => {
-    //     data_list = snapshot.val();
-    //     for (var i = 0; i < data_list.length; i++) {
-    //       console.log(data_list[i]);
-    //     }
-    //   });
+    firebase.database().ref("items").once("value").then((snapshot) => {
+        prev_data = snapshot.val();
+        if (prev_data.length === id_array.length) {
+          console.log("Everything is the same")
+          updateFirebase(data);
+        }
+        else if (prev_data.length > id_array.length) {
+          prev_data.map(function(i) {
+            if (id_array.includes(i.id) === false && i.model === "item") {
+              var msg = `_${i.name}_ was deleted off Viima`
+              var messages = {
+                text: msg,
+                channel: config.slack.channel,
+                attachments:[]
+              };
+              slack.notify(messages, function(err, result) {
+                if(err !== null) {
+                  console.log(err, result);
+                }
+              });
+            }
+          });
+          updateFirebase(data);
+        }
+        else {
+          var old_ids = prev_data.map(function(i) {
+            return i.id;
+          });
+          id_array.map(function(i) {
+            if (old_ids.includes(i) === false) {
+              data.map(function(ii) {
+                if (ii.id == i) {
+                  if (ii.model === "comment") {
+                    var msg = `${ii.fullname} added a ${ii.model} on Viima! Check it out: ${ii.url}`;
+                  }
+                  else {
+                    var msg = `${ii.fullname} added an ${ii.model} on Viima! Check it out: ${ii.url}`;
+                  }
+                  var messages = {
+                    text: msg,
+                    channel: config.slack.channel,
+                    attachments:[]
+                  };
+                  slack.notify(messages, function(err, result) {
+                    if(err !== null) {
+                      console.log(err, result);
+                    }
+                  });
+                }
+              });
+            }
+          });
+          updateFirebase(data);
+        }
+      });
 
-
-    // for (var i = 0; i < data.length; i++) {
-    //   console.log(data[i].item, data[i].model, data[i].id, data[i].name);
-
-      // firebase.database().ref(`items/${i}`).set(
-      //   data[i].id
-      // );
-      // console.log("firebase updated");
     // var slack_message = {
     //   text: data.fullname + " created " + data.name + " on Viima! Check it out at " + data.url,
     //   channel: config.slack.channel
@@ -92,5 +133,15 @@ function getViimaInfo() {
     //     console.log(err, result);
     //   }
     // });
-  });
+  // }
+});
+}
+function updateFirebase (data) {
+  for (var i = 0; i < data.length; i++) {
+
+    firebase.database().ref(`items/`).set(
+      data
+    );
+    console.log("firebase updated");
+  }
 }
