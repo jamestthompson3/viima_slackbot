30dc4b451145a14237866f625f95de131259ca9f
jamestthompson3 <jamestthompson3@gmail.com>
2017-07-02 12:35:04 -0700

    adding more slash commands

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ var express = require("express");
 var config = require("./config");
 var https = require("https");
 var Client = require('node-rest-client').Client;
-var parseJson = require('parse-json');
-var json = '{\n\t"foo": true,\n}';
+var firebase = require('firebase');
+
+var fire_config = {
+  apiKey: "AIzaSyDlVBhcGfHc3PfgKdiqoRud44AfDfvB5Dg",
+  authDomain: "viimabot-c9f70.firebaseapp.com",
+  databaseURL: "https://viimabot-c9f70.firebaseio.com",
+  storageBucket: "viimabot-c9f70.appspot.com",
+};
+firebase.initializeApp(fire_config);
 
 // Init Slack client
 
@@ -17,6 +24,7 @@ slack = new Slack(config.slack.webhook_url, {
 var app = express();
 
 app.get('/', function(request, res) {
+  res.send("Hi there!")
 });
 
 app.listen(config.api.port, function() {
@@ -29,7 +37,11 @@ app.post("/hello", function(request, res) {
     res.send("slack message sent")
   });
 });
-app.get("/test", function(request ,res) {
+app.post("/test", function(request, res) {
+  console.log(request);
+  res.send("heyyyy");
+});
+app.get("/update", function(request ,res) {
   getViimaInfo(function(result) {
     res.send("slack message sent")
   });
@@ -51,16 +63,34 @@ function postSlackThings(things, cb) {
 function getViimaInfo() {
   var client = new Client();
   client.get("https://app.viima.com/api/customers/1410/activities/", function (data, response) {
-    console.log(data.results.length);
-    var data = data.results[0]
-    var slack_message = {
-      text: data.fullname + " created " + data.name + " on Viima! Check it out at " + data.url,
-      channel: config.slack.channel
-    };
-    slack.notify(slack_message, function(err, result) {
-      if(err != null) {
-        console.log(err, result);
-      }
-    });
+    var data = data.results
+    var id_array = data.map(function(i) {
+      return i.id });
+    console.log(id_array);
+
+    // firebase.database().ref("items").once("value").then((snapshot) => {
+    //     data_list = snapshot.val();
+    //     for (var i = 0; i < data_list.length; i++) {
+    //       console.log(data_list[i]);
+    //     }
+    //   });
+
+
+    // for (var i = 0; i < data.length; i++) {
+    //   console.log(data[i].item, data[i].model, data[i].id, data[i].name);
+
+      // firebase.database().ref(`items/${i}`).set(
+      //   data[i].id
+      // );
+      // console.log("firebase updated");
+    // var slack_message = {
+    //   text: data.fullname + " created " + data.name + " on Viima! Check it out at " + data.url,
+    //   channel: config.slack.channel
+    // };
+    // slack.notify(slack_message, function(err, result) {
+    //   if(err != null) {
+    //     console.log(err, result);
+    //   }
+    // });
   });
 }
---
+++ helpers/worker1.js
@@ -0,0 +1,9 @@
+var Client = require('node-rest-client').Client;
+var firebase = require('firebase');
+var config = {
+  apiKey: "AIzaSyDlVBhcGfHc3PfgKdiqoRud44AfDfvB5Dg",
+  authDomain: "viimabot-c9f70.firebaseapp.com",
+  databaseURL: "https://viimabot-c9f70.firebaseio.com",
+  storageBucket: "viimabot-c9f70.appspot.com",
+};
+firebase.initializeApp(config);
